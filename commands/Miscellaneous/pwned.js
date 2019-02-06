3fbc2569f003169d548b67f17de4e09b16434a49
Alex <kovacs.alex26@gmail.com>
2019-02-06 18:19:14 +0200

    'uh'

--- commands/Miscellaneous/pwned.js
+++ commands/Miscellaneous/pwned.js
@@ -35,7 +35,7 @@ module.exports = class extends Command {
                 'User-Agent': `${this.client.user.username} (${this.client.user.id})`
             }
         });
-        if (res.status !== 200) return message.send("Something went wrong or there's no breaches linked to your account.")
+        if (res.status != 200) return message.send("Something went wrong or there's no breaches linked to your account.")
         if (res.status == 200) res = await res.json();
         const display = new RichDisplay(new MessageEmbed().setAuthor(`Account Breaches for ${email.input}`, message.author.displayAvatarURL({ size: 2048 })).setColor('#508ff4'));
         const time = new Timestamp('LLL');
