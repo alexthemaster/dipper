021ae1622c11a458008441716aea1ba71aace209
Alex <kovacs.alex26@gmail.com>
2019-02-23 09:11:39 +0200

    yes

--- bot.js
+++ bot.js
@@ -3,8 +3,6 @@ const config = require('./data/config.json');
 
 config.permissionLevels = new PermissionLevels()
     .add(0, () => true)
-    .add(4, ({ guild, member }) => guild && member.permissions.has('KICK_MEMBERS'), { fetch: true })
-    .add(5, ({ guild, member }) => guild && member.permissions.has('BAN_MEMBERS'), { fetch: true })
     .add(6, ({ guild, member }) => guild && member.permissions.has('ADMINISTRATOR'), { fetch: true })
     .add(7, ({ guild, member }) => guild && member === guild.owner, { fetch: true })
     .add(9, ({ author, client }) => author === client.owner, { break: true })
