4c52848b32cf683548ef4c6149439b2146542e5e
Alex <kovacs.alex26@gmail.com>
2019-02-24 17:39:01 +0200

    use og code for messageDelete and messageUpdate

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -1,25 +1,17 @@
-const { Event, Timestamp } = require('klasa');
+const { Event } = require('klasa');
 
 module.exports = class extends Event {
 
-    constructor(...args) {
-        super(...args, {
-            name: 'messageDelete',
-            enabled: true,
-            event: 'messageDelete',
-            once: false
-        });
-    }
-
-    async run(message) {
-        const settings = message.guild.settings.logs;
+	run(message) {
+		if (message.command && message.command.deletable) {
+			for (const msg of message.responses) {
+				msg.delete();
+			}
+		}
+		const settings = message.guild.settings.logs;
         if (settings.channel && settings.messageDelete) {
             return this.client.channels.get(settings.channel).send(`Message Deleted! (event: messageDelete)\n**Author** → ${message.author.tag} (${message.author.id})\n**Message Content** → \`${message.cleanContent}\` (${message.id})\n${message.attachments.size >= 1 ? `**Attachments** → ${message.attachments.size} attachments\n` : ''}**Channel** → ${message.channel.name} (${message.channel.id})\n**Created At** → ${new Timestamp('LLL').display(message.createdTimestamp)}\n**Deleted At** → ${new Timestamp('LLL').display(new Date())}\n--------------------`)
         }
-    }
-
-    async init() {
-
-    }
+	}
 
 };
--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -1,26 +1,14 @@
-const { Event, Timestamp } = require('klasa');
+const { Event } = require('klasa');
 
 module.exports = class extends Event {
 
-    constructor(...args) {
-        super(...args, {
-            name: 'messageUpdate',
-            enabled: true,
-            event: 'messageUpdate',
-            once: false
-        });
-    }
-
-    async run(oMessage, nMessage) {
-        const settings = oMessage.guild.settings.logs;
+	async run(old, message) {
+		if (this.client.ready && old.content !== message.content) this.client.monitors.run(message);
+		const settings = old.guild.settings.logs;
         if (settings.channel && settings.messageUpdate) {
-            if (oMessage.content === nMessage.content) return;
-            return this.client.channels.get(settings.channel).send(`Message Updated! (event: messageUpdate)\n**Author** → ${oMessage.author.tag} (${oMessage.author.id})\n**Old Message Content** → \`${oMessage.cleanContent}\`\n**New Message Content** → \`${nMessage.cleanContent}\`\n**Channel** → ${oMessage.channel.name} (${oMessage.channel.id})\n**Created At** → ${new Timestamp('LLL').display(oMessage.createdTimestamp)}\n**Updated At** → ${new Timestamp('LLL').display(new Date())}\n--------------------`)
+            if (old.content === message.content) return;
+            return this.client.channels.get(settings.channel).send(`Message Updated! (event: messageUpdate)\n**Author** → ${old.author.tag} (${old.author.id})\n**Old Message Content** → \`${old.cleanContent}\`\n**New Message Content** → \`${message.cleanContent}\`\n**Channel** → ${old.channel.name} (${old.channel.id})\n**Created At** → ${new Timestamp('LLL').display(old.createdTimestamp)}\n**Updated At** → ${new Timestamp('LLL').display(new Date())}\n--------------------`)
         }
-    }
-
-    async init() {
-
-    }
+	}
 
 };
