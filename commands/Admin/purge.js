485ad17c641848034bec6ad8f9c8d1ecd34839a8
Alex <kovacs.alex26@gmail.com>
2019-03-09 17:14:21 +0200

    fix purge

--- commands/Admin/purge.js
+++ commands/Admin/purge.js
@@ -19,14 +19,16 @@ module.exports = class extends Command {
             subcommands: false,
             description: 'Purge a given number of messages!',
             quotedStringSupport: false,
-            usage: '<messages:number>',
+            usage: '<messages:number{1,100}>',
             usageDelim: undefined,
             extendedHelp: 'No extended help available.'
         });
+
+        this.customizeResponse('messages', "Please specify a number between 1 and 100 (of messages) you want deleted!")
     }
 
     async run(message, [messages]) {
-        if (!message.member.permissions.has('DELETE_MESSAGES')) throw "You don't have permission to do this.";
+        if (!message.member.permissions.has('MANAGE_MESSAGES')) throw "You don't have permission to do this.";
         message.channel.bulkDelete(messages, true).then(msgs => {
             return message.send(`Successfully purged ${msgs.size} messages!`);
         }).catch(err => {
