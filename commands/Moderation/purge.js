364b8a4ac59478bf0fe1a37060cf144b9b512c96
Alex <kovacs.alex26@gmail.com>
2019-03-23 10:45:46 +0200

    purge messages from specific members

--- commands/Moderation/purge.js
+++ commands/Moderation/purge.js
@@ -19,18 +19,23 @@ module.exports = class extends Command {
             subcommands: false,
             description: 'Purge a given number of messages!',
             quotedStringSupport: false,
-            usage: '<messages:number{1,100}>',
-            usageDelim: undefined,
+            usage: '[member:member] <messages:number{1,100}>',
+            usageDelim: ' ',
             extendedHelp: 'No extended help available.'
         });
 
         this.customizeResponse('messages', "Please specify a number between 1 and 100 (of messages) you want deleted!")
     }
 
-    async run(message, [messages]) {
+    async run(message, [member, messages]) {
         if (!message.member.permissions.has('MANAGE_MESSAGES')) throw "You don't have permission to do this.";
-        message.channel.bulkDelete(messages, true).then(msgs => {
-            return message.send(`Successfully purged ${msgs.size} messages!`);
+        let msgs;
+        if (member) {
+            msgs = await message.channel.messages.fetch();
+            msgs = msgs.filter(msg => msg.author.id === member.user.id).first(messages);
+        }
+        message.channel.bulkDelete(msgs ? msgs : messages, true).then(msgsPurged => {
+            return message.send(`Successfully purged ${msgsPurged.size} messages${member ? ` from ${member.user.tag}` : ''}!`);
         }).catch(err => {
             return message.send(`An error occurred while performing this action.`);
         })
